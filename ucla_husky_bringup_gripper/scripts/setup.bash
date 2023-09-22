ef3bb09bc66d5d4f32b317dec7c1df4c9945ff69
Vivek <vivekalumootil@gmail.com>
2023-09-22 15:30:55 -0700

    first commit

--- ucla_husky_bringup_gripper/scripts/setup.bash
+++ ucla_husky_bringup_gripper/scripts/setup.bash
@@ -1,7 +1,15 @@
+#source ~/ucla_ws/src/ucla_husky/ucla_husky_bringup/scripts/setup.bash
+
+#--Dual-Arm----
 #export HUSKY_URDF_EXTRAS=$(catkin_find husky_ur_description urdf/husky_dual_ur5_e_description.urdf.xacro --first-only)
 
-export HUSKY_URDF_EXTRAS=$(catkin_find husky_ur_description urdf/husky_ur5_e_description.urdf.xacro --first-only)
 
 
-#export HUSKY_LEFT_UR_GRIPPER_ENABLED=0
-#export HUSKY_RIGHT_UR_GRIPPER_ENABLED=0
+#--Single-Arm--
+#export HUSKY_URDF_EXTRAS=$(catkin_find husky_ur_description urdf/husky_ur5_e_description.urdf.xacro --first-only)
+# roslaunch ucla_husky_bringup simulation.launch
+
+
+##--Single-Arm With Gripper--
+export HUSKY_URDF_EXTRAS=$(catkin_find husky_ur_description urdf/husky_ur5_e_gripper_description.urdf.xacro --first-only)
+# roslaunch ucla_husky_bringup_gripper simulation.launch
